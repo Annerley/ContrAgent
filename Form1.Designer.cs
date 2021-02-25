ef9d8b9a29c28c9a7feba1c584de75c2acfe3800
Дарья Лапутина <laputina975@gmail.com>
2021-02-25 16:59:29 +0300

    added logo

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,20 +29,27 @@ namespace ContrAgent
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.panel1 = new System.Windows.Forms.Panel();
-            this.inn = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.inn = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.Color.Yellow;
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(209)))), ((int)(((byte)(161)))));
+            this.panel1.Controls.Add(this.textBox4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.panel2);
             this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.textBox3);
             this.panel1.Controls.Add(this.label2);
@@ -56,52 +63,53 @@ namespace ContrAgent
             this.panel1.Size = new System.Drawing.Size(1038, 600);
             this.panel1.TabIndex = 0;
             // 
-            // inn
+            // textBox4
             // 
-            this.inn.AutoSize = true;
-            this.inn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.inn.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.inn.Location = new System.Drawing.Point(12, 9);
-            this.inn.Name = "inn";
-            this.inn.Size = new System.Drawing.Size(78, 31);
-            this.inn.TabIndex = 0;
-            this.inn.Text = "ИНН:";
+            this.textBox4.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox4.Location = new System.Drawing.Point(560, 264);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(142, 28);
+            this.textBox4.TabIndex = 9;
             // 
-            // textBox1
+            // label3
             // 
-            this.textBox1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox1.Location = new System.Drawing.Point(227, 12);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(142, 28);
-            this.textBox1.TabIndex = 1;
-           
+            this.label3.AutoSize = true;
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(388, 264);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(154, 31);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Номер СЭД:";
             // 
-            // textBox2
+            // panel2
             // 
-            this.textBox2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox2.Location = new System.Drawing.Point(227, 85);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(142, 28);
-            this.textBox2.TabIndex = 3;
-         
+            this.panel2.BackgroundImage = global::ContrAgent.Properties.Resources.logo;
+            this.panel2.Location = new System.Drawing.Point(867, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(170, 170);
+            this.panel2.TabIndex = 7;
             // 
-            // label1
+            // button1
             // 
-            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.label1.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(12, 61);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(161, 86);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Номер заключения:";
-            this.label1.UseMnemonic = false;
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(172)))), ((int)(((byte)(132)))));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(838, 526);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(188, 62);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Закинуть в базу";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox3
             // 
             this.textBox3.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox3.Location = new System.Drawing.Point(227, 137);
+            this.textBox3.Location = new System.Drawing.Point(122, 161);
             this.textBox3.Multiline = true;
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(142, 28);
@@ -112,27 +120,51 @@ namespace ContrAgent
             this.label2.AutoSize = true;
             this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.label2.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(12, 137);
+            this.label2.Location = new System.Drawing.Point(12, 259);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(187, 31);
             this.label2.TabIndex = 4;
             this.label2.Text = "Дата проверки:";
-            
             // 
-            // button1
+            // textBox2
             // 
-            this.button1.BackColor = System.Drawing.Color.LimeGreen;
-            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.button1.Location = new System.Drawing.Point(181, 228);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(188, 62);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "Закинуть в базу";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.textBox2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(487, 161);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(142, 28);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(305, 156);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 86);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Номер заключения:";
+            this.label1.UseMnemonic = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(205, 264);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(142, 28);
+            this.textBox1.TabIndex = 1;
+            // 
+            // inn
+            // 
+            this.inn.AutoSize = true;
+            this.inn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.inn.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.inn.Location = new System.Drawing.Point(12, 161);
+            this.inn.Name = "inn";
+            this.inn.Size = new System.Drawing.Size(78, 31);
+            this.inn.TabIndex = 0;
+            this.inn.Text = "ИНН:";
             // 
             // Form1
             // 
@@ -140,8 +172,9 @@ namespace ContrAgent
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1038, 600);
             this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "ПО \"Контрагент\"";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.ResumeLayout(false);
@@ -158,6 +191,9 @@ namespace ContrAgent
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label inn;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,10 +18,36 @@ namespace ContrAgent
             InitializeComponent();
         }
 
-
         private void button1_Click(object sender, EventArgs e)
         {
+            
+
+
+            DB db = new DB();
+
+            DataTable table = new DataTable();
+
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+
+            MySqlCommand command = new MySqlCommand("INSERT INTO `books` (`name`, `year`) VALUES (@name, @year)", db.getConnection());
+            command.Parameters.Add("@name", MySqlDbType.VarChar).Value = textBox1.Text;
+            command.Parameters.Add("@year", MySqlDbType.Int32).Value = textBox2.Text;
+
+            db.openConnection();
+            if (command.ExecuteNonQuery() == 1)
+                MessageBox.Show("Добавилось");
+            else
+                MessageBox.Show("Не добавилось");
+
+            db.closeConnection();
+
+            
+            //для select
+            //adapter.Fill(table);
+
 
         }
+
+       
     }
 }
