929d688f26491495779ceb20cc58200e64a8e0cf
Дарья Лапутина <laputina975@gmail.com>
2021-02-25 15:19:12 +0300

    Добавлена БД

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,11 @@ namespace ContrAgent
         {
             InitializeComponent();
         }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
